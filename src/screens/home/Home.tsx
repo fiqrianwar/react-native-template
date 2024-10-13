2237c6573d914c96202cda1cc224e8c2b96c5b9b
fiqrianwar <fiqrianwar01@gmail.com>
2024-10-13 20:10:25 +0700

    implement stylesheet react native

--- src/screens/home/Home.tsx
+++ src/screens/home/Home.tsx
@@ -2,16 +2,13 @@ import {View, Button} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {StackNavigationProps} from '../../navigation/types';
+import styles from './styles';
 
 const Home = () => {
   const navigation = useNavigation<StackNavigationProps>();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-      }}>
+    <View style={styles.container}>
       <Button
         title="Direct To Profile"
         onPress={() =>
--- src/screens/profile/Profile.tsx
+++ src/screens/profile/Profile.tsx
@@ -2,18 +2,15 @@ import {View, Button} from 'react-native';
 import React from 'react';
 import {useNavigation} from '@react-navigation/native';
 import {TabNavigationProps} from '../../navigation/types';
+import styles from './styles';
 
 const Profile = () => {
   const navigation = useNavigation<TabNavigationProps>();
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: 'center',
-      }}>
+    <View style={styles.container}>
       <Button
-        title="Direct To Home"
+        title="Direct To More Menu"
         onPress={() =>
           navigation.navigate('MainTabNavigator', {
             screen: 'MoreMenuScreen',
